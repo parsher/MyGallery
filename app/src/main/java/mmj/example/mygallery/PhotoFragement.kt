0588ba94c429bfeb3965f03de1115dc42f135063
parsher <futureslabor@gmail.com>
2019-06-09 10:04:28 +0900

    Initial commit

---
+++ app/src/main/java/mmj/example/mygallery/PhotoFragement.kt
@@ -0,0 +1,47 @@
+package mmj.example.mygallery
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.fragment_photo_fragement.*
+
+
+private const val ARG_URI = "uri"
+
+class PhotoFragement : Fragment() {
+    private var uri: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            uri = it.getString(ARG_URI)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_photo_fragement, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Glide.with(this).load(uri).into(imageView)
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance(uri: String) =
+            PhotoFragement().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_URI, uri)
+                }
+            }
+    }
+}
